e8de2c8b5c4016f36aaedcb4f26f52c288c986c2
Breno0101Kau <127498625+Breno0101Kau@users.noreply.github.com>
2023-11-10 08:59:16 -0300

    Av. Bimestre 3
    Breno e Emili

---
+++ Downloads/main (1).js
@@ -0,0 +1,46 @@
+function tocaSomAplausos (){
+    document.querySelector('#som_tecla_aplausos').play();
+}
+document.querySelector('.tecla_aplausos').onclick = tocaSomAplausos;
+
+function tocaSomVaia (){
+    document.querySelector('#som_tecla_vaia').play();
+}
+document.querySelector('.tecla_vaia').onclick = tocaSomVaia;
+
+function tocaSomRisada (){
+    document.querySelector('#som_tecla_risada').play();
+}
+document.querySelector('.tecla_risada').onclick = tocaSomRisada;
+
+function tocaSomErrado (){
+    document.querySelector('#som_tecla_errado').play();
+}
+document.querySelector('.tecla_errado').onclick = tocaSomErrado;
+
+function tocaSomGrilo (){
+    document.querySelector('#som_tecla_grilo').play();
+}
+document.querySelector('.tecla_grilo').onclick = tocaSomGrilo;
+
+function tocaSomTambores (){
+    document.querySelector('#som_tecla_tambores').play();
+}
+document.querySelector('.tecla_tambores').onclick = tocaSomTambores;
+
+function tocaSomTrombeta (){
+    document.querySelector('#som_tecla_trombeta').play();
+}
+document.querySelector('.tecla_trombeta').onclick = tocaSomTrombeta;
+
+function tocaSomDinheiro (){
+    document.querySelector('#som_tecla_dinheiro').play();
+}
+document.querySelector('.tecla_dinheiro').onclick = tocaSomDinheiro;
+
+function tocaSomVitória (){
+    document.querySelector('#som_tecla_vitória').play();
+}
+document.querySelector('.tecla_vitória').onclick = tocaSomVitória;
+
+
